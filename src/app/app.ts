874c26138f0b5f8694de907a7b8d1efc1184ac96
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-06-30 13:49:08 +0600

    refactor: remove unused title property from App component

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'diucse-alumni-public';
-}
+export class App {}
