a4c8948abb4e8222251c8871ecf40aa7ace1fb94
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-07-01 18:52:55 +0600

    feat: implement portal layout with navigation links and dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,105 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    // Authentication
+    // {
+    //     path: 'login',
+    //     title: 'Login - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'register',
+    //     title: 'Register - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'forgot-password',
+    //     title: 'Forgot Password - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'reset-password',
+    //     title: 'Reset Password - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'verify-email',
+    //     title: 'Verify Email - DIUCSE Alumni',
+    // },
+
+    // // Events
+    // {
+    //     path: 'events',
+    //     title: 'Events - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'events/:id',
+    //     title: 'Event Details - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'events/:id/register',
+    //     title: 'Register for Event - DIUCSE Alumni',
+    // },
+
+    // // Blogs
+    // {
+    //     path: 'blogs',
+    //     title: 'Blogs - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'blogs/:id',
+    //     title: 'Blog Details - DIUCSE Alumni',
+    // },
+
+    // // Misc
+    // {
+    //     path: 'about',
+    //     title: 'About Us - DIUCSE Alumni',
+    // },
+    // {
+    //     path: 'contact',
+    //     title: 'Contact Us - DIUCSE Alumni',
+    // },
+
+
+    // Portal
+    {
+        path: 'portal',
+        loadComponent: () => import('./pages').then(c => c.Portal),
+        title: 'Portal - DIUCSE Alumni',
+        children: [
+            {
+                path: 'dashboard',
+                title: 'Dashboard - DIUCSE Alumni',
+                loadComponent: () => import('./pages').then(c => c.Dashboard)
+            },
+            // {
+            //     path: 'profile',
+            //     title: 'Profile - DIUCSE Alumni',
+            // },
+            // {
+            //     path: 'settings',
+            //     title: 'Settings - DIUCSE Alumni',
+            // },
+            // {
+            //     path: 'membership',
+            //     title: 'Membership - DIUCSE Alumni',
+            // },
+            // {
+            //     path: 'events',
+            //     title: 'Events - DIUCSE Alumni',
+            // },
+            // {
+            //     path: 'blogs',
+            //     title: 'Blogs - DIUCSE Alumni',
+            // },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
     {
         path: '',
         loadComponent: () => import('./pages').then(c => c.Home),
         title: 'Home - DIUCSE Alumni',
         pathMatch: 'full'
-    },
-    {
-        path: 'portal',
-        loadComponent: () => import('./pages').then(c => c.Portal),
-        title: 'Portal - DIUCSE Alumni',
     },
 ];
--- src/app/pages/portal/portal.ts
+++ src/app/pages/portal/portal.ts
@@ -1,9 +1,34 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { NgClass } from "@angular/common";
+import { httpResource } from "@angular/common/http";
+import { ChangeDetectionStrategy, Component, resource } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+
+interface Link {
+    title: string;
+    path: string;
+    icon?: string;
+    description?: string;
+}
 
 @Component({
     selector: 'portal',
     templateUrl: './portal.html',
     styleUrl: './portal.css',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive,
+        NgClass
+    ]
 })
-export class Portal {}
+export class Portal {
+    protected links = httpResource<Array<Link>>(() => '/data/portal-links.json');
+    protected collapsed = document.body.clientWidth < 768;
+
+    protected selected: Link | null = null;
+
+    protected onLinkClicked(link: Link) {
+        this.selected = link;
+    }
+}
