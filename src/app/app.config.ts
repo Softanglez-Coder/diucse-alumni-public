93972b6dc5b4cb2ea260a45f092b5634d97febb9
Mehedi Hasan <mehedi.hasan23@brainstation-23.com>
2025-08-10 16:37:39 +0600

    feat(config): update production base URL to use the correct API endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,7 +22,7 @@ function getBaseUrl(): string {
   if (isLocalhost) {
     baseUrl = 'http://localhost:3000';
   } else if (isProduction) {
-    baseUrl = 'https://csediualumni.com/api';
+    baseUrl = 'https://api.csediualumni.com';
   } else {
     baseUrl = 'http://localhost:3000'; // Fallback for other environments
   }
